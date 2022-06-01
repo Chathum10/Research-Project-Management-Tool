25c16ebef5268c628a1a922f023a96b8d03067af
Chathum10 <89167761+Chathum10@users.noreply.github.com>
2022-06-01 17:52:36 +0530

    Initial Commit
    update frontend & backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const usersRouter = require("./routes/api/users");
+const config = require('config');
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
+// Body parser middleware
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+// DB Config
+const db = config.get('mongoURI');
+// Connect to MongoDB
+mongoose
+    .connect(
+        db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", usersRouter);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+

989cb2ff051c7f869161954ee665801692de359e
Pramoda Piyumali <pramodapiyumali@gmail.com>
2022-06-04 18:52:44 +0530

    update

--- Frontend/src/Components/CoSupervisorView/AssignToGroup/CSviewTopics.js
+++ Frontend/src/Components/CoSupervisorView/AssignToGroup/CSviewTopics.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 export default class CSviewTopics extends Component {
   constructor(props) {
@@ -48,12 +50,16 @@ export default class CSviewTopics extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-9 mt-2 mb-2">
-            <h4>Topic List</h4>
+      <div>
+        <br /> <br /> <br />
+        <div id="containerJoin">
+          <center>
+            <h1>Topic List</h1>
+            </center>
           </div>
-          <div className="col-lg-3 mt-2 mb-2">
+
+          <div style={{ marginTop: 20 }} className="container">
+        <div style={{ width: "20%", marginLeft: "80%" }}>
             <input
               className="form-control"
               type="searh"
@@ -63,11 +69,11 @@ export default class CSviewTopics extends Component {
             ></input>
           </div>
         </div>
-
-        <table className="table">
+        <br />
+        <Table striped>
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th >#</th>
               <th>Group ID</th>
               <th>Research Area</th>
               <th>Topic</th>
@@ -76,20 +82,14 @@ export default class CSviewTopics extends Component {
               <th>Comments</th>
               <th>Supervisor's Name</th>
               <th>Co-Supervisor's Name</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {this.state.topics.map((topics, index) => (
               <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>
-                  <a
-                    href={`/topicRoutes/${topics._id}`}
-                    style={{ textDecoration: "none" }}
-                  >
-                    {topics.gid}
-                  </a>
-                </td>
+                <th >{index + 1}</th>
+                <td>{topics.gid}</td>
                 <td>{topics.area}</td>
                 <td>{topics.name}</td>
                 <td>{topics.description}</td>
@@ -98,16 +98,16 @@ export default class CSviewTopics extends Component {
                 <td>{topics.sName}</td>
                 <td>{topics.csName}</td>
                 <td>
-                  <a className="btn btn-warning" href={`/CSreply/${topics._id}`}>
-                    <i className="fas fa-edit"></i>&nbsp;Assign
-                  </a>
+                <Button variant="warning" size="sm">
+                  <a href={`/CSreply/${topics._id}`}>Assign</a>
                   &nbsp;
+                  </Button>
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>
-      </div>
+        </Table>
+    </div>
     );
   }
 }

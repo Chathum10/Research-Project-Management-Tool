5c4d0fde2c0191f7e9759192b7f533391002c3b8
Chathum10 <89435818+ashen910@users.noreply.github.com>
2022-06-04 20:41:29 +0530

    Update SupervisorReply.js

--- Frontend/src/Components/SupervisorView/AcceptTopics/SupervisorReply.js
+++ Frontend/src/Components/SupervisorView/AcceptTopics/SupervisorReply.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Card from 'react-bootstrap/Card';
 
 export default class SupervisorReply extends Component {
   constructor(props) {
@@ -83,144 +84,167 @@ export default class SupervisorReply extends Component {
 
   render() {
     return (
-      <div className="col-md-8 mt-4 mx-auto">
-        <h1 className="h3 mb-3 font-weight-normal">Topic Details</h1>
+      <div className="back fixed" style={{ zIndex: 8 }}><br />
+        <div className="com-md-8 mt-4 mx-auto"></div>
+        <br /> <br />
+        <center><h1>Topic Details</h1></center>
+        <center>
+          <br />
+          {[
+            'Secondary',
+          ].map((variant) => (
+            <Card bg={variant.toLowerCase()}
+              key={variant}
+              text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+              style={{ width: '40rem' }}
+              className="mb-2">
 
-        <form className="needs-validation" noValidate>
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Group ID</label>
-            <input
-              disabled
-              type="text"
-              className="form-control"
-              name="gid"
-              placeholder=""
-              value={this.state.gid}
-              onChange={this.handleInputChange}
-            />
+              <div className="col-md-8 mt-4 mx-auto">
+                <br />
+                <form className="needs-validation" noValidate>
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Group ID</b></label>
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      name="gid"
+                      placeholder=""
+                      value={this.state.gid}
+                      onChange={this.handleInputChange}
+                    />
 
-            <div className="form-group" style={{ marginBottom: "15px" }}>
-              <label style={{ marginBottom: "5px" }}>Research Area</label>
-              <select
-                disabled
-                name="area"
-                value={this.state.area}
-                onChange={this.handleInputChange}
-                className="form-control"
-              >
-                <option defaultValue>--Select Your Research Area--</option>
-                <option value="Cloud computing">Cloud computing</option>
-                <option value="Distributed system">Distributed system</option>
-                <option value="Green computing "> Green computing</option>
-                <option value="Data Warehousing "> Data Warehousing</option>
-                <option value="Artificial Intelligence ">
-                  {" "}
-                  Artificial Intelligence
-                </option>
-                <option value="Internet of Things ">Internet of Things </option>
-                <option value="Machine learning ">Machine learning </option>
-                <option value="Human Computer Interaction  ">
-                  {" "}
-                  Human Computer Interaction{" "}
-                </option>
-                <option value="Data mining semantic-web-mining ">
-                  Data mining semantic-web-mining{" "}
-                </option>
-                <option value="Software fault localization ">
-                  Software fault localization{" "}
-                </option>
-              </select>
-            </div>
-          </div>
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Sub Topic</label>
-            <input
-              disabled
-              type="text"
-              className="form-control"
-              name="name"
-              placeholder=""
-              value={this.state.name}
-              onChange={this.handleInputChange}
-            />
-          </div>
+                    <div className="form-group" style={{ marginBottom: "15px" }}>
+                      <label style={{ marginBottom: "5px" }}><b>Research Area</b></label>
+                      <select
+                        disabled
+                        name="area"
+                        value={this.state.area}
+                        onChange={this.handleInputChange}
+                        className="form-control"
+                      >
+                        <option defaultValue>--Select Your Research Area--</option>
+                        <option value="Cloud computing">Cloud computing</option>
+                        <option value="Distributed system">Distributed system</option>
+                        <option value="Green computing "> Green computing</option>
+                        <option value="Data Warehousing "> Data Warehousing</option>
+                        <option value="Artificial Intelligence ">
+                          {" "}
+                          Artificial Intelligence
+                        </option>
+                        <option value="Internet of Things ">Internet of Things </option>
+                        <option value="Machine learning ">Machine learning </option>
+                        <option value="Human Computer Interaction  ">
+                          {" "}
+                          Human Computer Interaction{" "}
+                        </option>
+                        <option value="Data mining semantic-web-mining ">
+                          Data mining semantic-web-mining{" "}
+                        </option>
+                        <option value="Software fault localization ">
+                          Software fault localization{" "}
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Sub Topic</b></label>
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      placeholder=""
+                      value={this.state.name}
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Brief Description</label>
-            <input
-              disabled
-              type="text"
-              className="form-control"
-              name="description"
-              placeholder=""
-              value={this.state.description}
-              onChange={this.handleInputChange}
-            />
-          </div>
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Brief Description</b></label>
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      name="description"
+                      placeholder=""
+                      value={this.state.description}
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Status</label>
-            <br />
-            <input
-              className="form-check-input"
-              type="radio"
-              name="status"
-              onChange={this.handleInputChange}
-              value="Approved"
-            />
-            <label class="form-check-label" for="flexRadioDefault1">
-              {" "}
-              Approved{" "}
-            </label>
-            &nbsp;&nbsp;
-            <input
-              className="form-check-input"
-              type="radio"
-              name="status"
-              onChange={this.handleInputChange}
-              value="Rejected"
-            />
-            <label class="form-check-label" for="flexRadioDefault2">
-              {" "}
-              Rejected{" "}
-            </label>
-            &nbsp;&nbsp;
-          </div>
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Comments</label>
-            <input
-              type="text"
-              className="form-control"
-              name="comment"
-              placeholder=""
-              value={this.state.comment}
-              onChange={this.handleInputChange}
-            />
-          </div>
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Status</b></label>
+                    <br />
+                    <input
+                      required
+                      className="form-check-input"
+                      type="radio"
+                      name="status"
+                      onChange={this.handleInputChange}
+                      value={this.state.status}
+                    />
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      {" "}
+                      Approved{" "}
+                    </label>
+                    &nbsp;&nbsp;
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="status"
+                      onChange={this.handleInputChange}
+                      value={this.state.status}
+                    />
+                    <label class="form-check-label" for="flexRadioDefault2">Rejected</label>
+                    &nbsp;&nbsp;
+                  </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Supervisor's Name</label>
-            <input
-              type="text"
-              className="form-control"
-              name="sName"
-              placeholder=""
-              value={this.state.sName}
-              onChange={this.handleInputChange}
-            />
-          </div>
 
-          <button
-            className="btn btn-success"
-            type="submit"
-            style={{ marginTop: "15px" }}
-            onClick={this.onSubmit}
-          >
-            <i className="far far-check-square"></i>
-            &nbsp; Reply to Thread{" "}
-          </button>
-        </form>
+
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Comments</b></label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="comment"
+                      placeholder=""
+                      value={this.state.comment}
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+
+                  <div className="form-group" style={{ marginBottom: "15px" }}>
+                    <label style={{ marginBottom: "5px" }}><b>Supervisor's Name</b></label>
+                    <input
+                      required
+                      type="text"
+                      className="form-control"
+                      name="sName"
+                      placeholder=""
+                      value={this.state.sName}
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+
+                  <button
+                    className="btn btn-success"
+                    type="submit"
+                    style={{ marginTop: "15px" }}
+                    onClick={this.onSubmit}
+                  >
+                    &nbsp;Publish
+                  </button>
+                  <br /> <br /> <br />
+                </form>
+              </div>
+            </Card>
+          ))}
+          <br /> <br /> <br /> <br />
+        </center>
       </div>
+
+
     );
   }
 }
